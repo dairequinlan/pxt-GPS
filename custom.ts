730bbc5f413ac57aca60d884b850f16ee9a408e6
Daire Quinlan <daire.quinlan@gmail.com>
2023-03-23 12:42:20 +0000

    test blocks

---
+++ custom.ts
@@ -0,0 +1,23 @@
+
+//% weight=100 color=#0fbc11 icon=""
+namespace GPS {
+    /**
+     * TODO: describe your function here
+     * @param n describe parameter here, eg: 5
+     * @param s describe parameter here, eg: "Hello"
+     * @param e describe parameter here
+     */
+    //% block
+    export function foo(): string {
+        return "something";
+    }
+
+    /**
+     * TODO: describe your function here
+     * @param value describe value here, eg: 5
+     */
+    //% block
+    export function anotherOne(value: number): number {
+        return value + 1;
+    }
+}
--- main.ts
+++ main.ts
@@ -1,1 +1,6 @@
-
+let bob = 0
+basic.forever(function () {
+    bob = GPS.anotherOne(bob)
+    basic.showNumber(bob)
+    basic.pause(100)
+})
